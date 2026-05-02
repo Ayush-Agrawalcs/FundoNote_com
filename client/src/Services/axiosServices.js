9656cb934bcb66125a67ab4b338f0bf16eb4175d
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2026-05-02 14:29:36 +0530

    Add

--- client/src/Services/axiosServices.js
+++ client/src/Services/axiosServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000", // your FastAPI server
+    baseURL: "https://fundonote-com-7.onrender.com", // ✅ your deployed backend
     headers: {
         "Content-Type": "application/json",
     },
